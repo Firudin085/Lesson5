565bb1953a5423b42073b379aff3fc64df67a88f
Firudin <98107652+Firudin085@users.noreply.github.com>
2024-03-25 19:50:57 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,45 +1,92 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+
+            *
+           ***
+          *****
+         *******
+        *********
+
+         */
 
 
-        int[] arr = {1,2,7,5,0};
-
-
-        //Bubble Sort istifadesi
-
-        for ( int i = 0 ; i <arr.length - 1; i ++){
-            for (int k = 0; k < arr.length - i - 1; k++){
-                if (arr[k] > arr[k+1]){
-                    int T = arr[k];
-                    arr[k] = arr[k+1];
-                    arr[k+1] = T;
-                }
+        for ( int i = 1; i<=5 ; i++){
+            for (int k = i; k <=4 ;k++){
+                System.out.print(" ");
             }
-        }
-        for (int i = 0 ; i<arr.length; i++){
-            System.out.print(" "+arr[i]);
+
+            for (int j = 0; j < i*2-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
         }
 
 
-//        // verilen Arrayimizi men selection sort ve bubble alqoritmasiynan etmeye calisacam
+
+
+
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// BUBBLE SORT
+//        int n = arr.length;
+//        for (int i = 0 ; i < n - 1;i++ ){
+//            for (int k = 0;k <n-i-1;k++){
+//                if (arr[k] > arr[k+1]){
+//                    int temp = arr[k];
+//                    arr[k]  = arr[k+1];
+//                    arr[k+1] = temp;
+//                }
+//            }
 //
-//        //Selection Sort
+//        }
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(arr[i]);
+//        }
 //
-//        for (int i = 0 ; i<arr.length-1 ; i++){
-//            int min =i;
 //
-//            for (int k = i +1 ; k < arr.length; k++){
+//
+
+
+
+// SELECTION SORT
+//        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+//        int i,k;
+//        for (i = 0 ; i <arr.length-1 ; i ++){
+//            int min = i;
+//            for (k = i + 1; k < arr.length; k++){
 //                if (arr[k] < arr[min]){
 //                    min = k;
 //                }
 //            }
-//            int T = arr[min];
+//            int temp = arr[min];
 //            arr[min] = arr[i];
-//            arr[i] = T;
+//             arr[i] = temp;
+//
+//            System.out.println(arr[i]);
 //        }
 //
-//        for (int i = 0 ; i<arr.length; i++){
-//            System.out.print(" "+ arr[i]);
-//        }
-    }
-}
+
